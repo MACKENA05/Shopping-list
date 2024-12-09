a87858a3b9a6c64fadede123311833110c50abd9
MonicahMackena¨ <mackenamuthui@gmail.com>
2024-12-09 22:41:56 +0300

    created a function to generate the shoppinglist onto the webpage

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ const addButton = document.getElementById('addbutton');
 const clearButton = document.getElementById('clearButton');
 const shoppingListContainer =document.getElementById('shoppingList');
 
+//function to generate shopping list
+function generateList(){
+    shoppingListContainer.innerHTML = '';  //clear the list
+    shoppingList.foreach((item,index)=>{
+        const li = document.createElement('li');
+        li.classLis.toggle('purchased',item.purchased);                                          //Toggle 'purchased' class based on status
+        //setting inner HTML
+        li.innerHTML = `
+        <input type="checkbox" class="checkbox" data-index="${index}" ${item.purchased ? 'checked' : ''}>
+        <span contenteditable="true" class="item-text">${item.text}</span>`;
+        
+      shoppingListContainer.appendChild(li);                                                           //append <li> item to the <ul> container.
+    });
+ 
