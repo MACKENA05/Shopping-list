f8134077303afe694c10a4f3e4a510d8245868c5
MonicahMackena¨ <mackenamuthui@gmail.com>
2024-12-09 22:36:28 +0300

    select DOM elements

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //retriving the shopping list from local storage
 const shoppingList = JSON.parse(localStorage.getItem('shoppingList')) || [];
+// Selecting DOM elements
+const itemInput = document.getElementById('itemInput');
+const addButton = document.getElementById('addbutton');
+const clearButton = document.getElementById('clearButton');
+const shoppingListContainer =document.getElementById('shoppingList');
+
